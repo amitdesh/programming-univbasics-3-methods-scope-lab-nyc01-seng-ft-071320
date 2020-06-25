eb2e383377f766e20228f1451be4385c4222b3ed
A D <amitdesh2014@gmail.com>
2020-06-25 18:32:43 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,6 @@
 phrase = "It's-a me, Mario!"
 status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+link = "It's Dangerous To Go Alone! Take This."
 
 def mario
     puts phrase
@@ -10,4 +11,11 @@ def toadstool
 end
 
 def link
-  puts ""
+  puts link
+end
+
+def all_phrases
+  puts phrase
+  puts status
+  puts link
+end
