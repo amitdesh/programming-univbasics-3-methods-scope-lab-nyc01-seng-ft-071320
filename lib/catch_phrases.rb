cf4f26da3ccb31162108f5dbd02675f6c336b2b2
A D <amitdesh2014@gmail.com>
2020-06-25 18:37:08 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,10 +8,12 @@ def mario
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
 def link
+  link_words = "It's Dangerous To Go Alone! Take This."
   puts link_words
 end
 
